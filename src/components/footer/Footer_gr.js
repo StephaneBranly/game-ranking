abb1cc9d6edb13559791122cda16f9bfa663be1d
Stephane_Branly <stephanebranly.pro@gmail.com>
2021-08-29 12:46:05 +0200

    add: first test

---
+++ src/components/footer/Footer_gr.js
@@ -0,0 +1,13 @@
+import './Footer_gr.css';
+
+function Footer_gr() {
+  return (
+   <footer>
+       <div id='content'>
+            Buy me a coffee :)
+       </div>
+   </footer>
+  );
+}
+
+export default Footer_gr;
---
+++ src/components/header/Header_gr.js
@@ -0,0 +1,11 @@
+import './Header_gr.css';
+
+function Header_gr() {
+  return (
+   <header>
+       Welcome to Game Ranking!
+   </header>
+  );
+}
+
+export default Header_gr;
