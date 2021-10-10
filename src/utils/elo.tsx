229c792374b766351dac6537cca11634c7707ced
Stephane_Branly <stephanebranly.pro@gmail.com>
2021-10-10 18:01:30 +0200

    fix: K factor

--- src/utils/elo.tsx
+++ src/utils/elo.tsx
@@ -3,5 +3,5 @@ export const elo_expected = (A: number, B: number): number => {
 }
 
 export const elo = (old: number, exp: number, score: 0 | 1, nbTeam:number=1) => {
-    return old + 32 * (score - exp) / nbTeam
+    return old + 20 * (score - exp) / nbTeam
 }
