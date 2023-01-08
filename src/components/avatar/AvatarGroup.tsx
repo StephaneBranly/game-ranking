ee9b21758497eecb0aa5d5d38e079fbdce271d50
Stephane_Branly <stephanebranly.pro@gmail.com>
2023-01-08 15:44:54 -0500

    [~] Fix more avatar group

--- src/components/avatar/AvatarGroup.tsx
+++ src/components/avatar/AvatarGroup.tsx
@@ -9,9 +9,9 @@ const AvatarGroup = (props: AvatarGroupProps) => {
     return (
         <div className='avatar-group'>
             {props.children.slice(0,props.max).map((child,index) => {
-                return <div className='avatar-group__avatar' key={index}>{child}</div>
+                return <div className='avatar-group-avatar' key={index}>{child}</div>
             })}
-            {props.children.length > props.max && <div className='avatar-group__avatar avatar-group__avatar--more'>+{props.children.length - props.max}</div>}
+            {props.children.length > props.max && <div className='avatar-group-avatar-more'>+{props.children.length - props.max}</div>}
         </div>
     )
 }
